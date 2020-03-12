cff5a08c4c293e5fb867cfb1286fbc16fe7af631
knoxgon <volkan.guven@hotmail.se>
2020-03-12 12:01:17 +0100

    Reset repository due severe security vulnerability

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.send({message: 'User response'});
-});
-
-module.exports = router;
